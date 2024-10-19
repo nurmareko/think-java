593f24bb36faf6ecb2b0fb8464ecf6e910f056a8
nurmareko <nurmareko.college@gmail.com>
2024-10-19 21:43:16 +0700

    add exercise 1-1, 1-2, 1-3

---
+++ 1-2/Hello.java
@@ -0,0 +1,12 @@
+//comment
+public class Hello { //comment
+    //comment
+    public static void main(String[] args) { // comment
+        // comment
+        System.out.println("Hello, World!"); // comment
+        // comment
+        System.out.println("How are you?"); // comment
+        //comment
+    } // comment
+} // comment
+// comment
